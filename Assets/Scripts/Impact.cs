646bdea9319cd109e10127dc8b32a15351b1a5fa
Arturo Nereu <hdgam3r@gmail.com>
2020-05-18 19:58:44 -0500

    Added Basic Deformation Shader

---
+++ Assets/Scripts/Impact.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impact : MonoBehaviour
+{
+    private Camera m_MainCamera;
+
+    private Material m_Material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_MainCamera = Camera.main;
+        m_Material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = m_MainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+            
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                m_Material.SetVector("Vector3_B0DDBEEE", raycastHit.point);
+                Debug.Log("Input: " + raycastHit.point);
+            }
+        }
+    }
+}
