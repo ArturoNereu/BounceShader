191b9316e85ef88f3986aec5c444454895c1ded5
Arturo Nereu <hdgam3r@gmail.com>
2020-05-24 14:11:13 -0500

    Added Sheriff's House

--- Assets/Scripts/Impact.cs
+++ Assets/Scripts/Impact.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Impact : MonoBehaviour
 {
@@ -8,26 +6,50 @@ public class Impact : MonoBehaviour
 
     private Material m_Material;
 
+    private int m_RaycastHitHash;
+    private int m_ImpactValueAtCurveHash;
+
+    private float m_TimeSincePressed;
+
+    public AnimationCurve m_AnimationCurve;
+
+
     // Start is called before the first frame update
     void Start()
     {
         m_MainCamera = Camera.main;
         m_Material = GetComponent<Renderer>().material;
+        m_RaycastHitHash = Shader.PropertyToID("Vector3_B0DDBEEE");
+        m_ImpactValueAtCurveHash = Shader.PropertyToID("Vector1_E44E3D54");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            m_TimeSincePressed = 0;
+
             Ray ray = m_MainCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit raycastHit;
             
             if(Physics.Raycast(ray, out raycastHit))
             {
-                m_Material.SetVector("Vector3_B0DDBEEE", raycastHit.point);
-                Debug.Log("Input: " + raycastHit.point);
+                m_Material.SetVector(m_RaycastHitHash, raycastHit.transform.position);
+            }
+            else
+            {
+                // TODO: Remove 
+                m_Material.SetVector(m_RaycastHitHash, new Vector4(1000,1000,1000,100)) ;
             }
         }
+
+        m_TimeSincePressed += Time.deltaTime;
+        m_Material.SetFloat(m_ImpactValueAtCurveHash, EvaluateCurve(m_TimeSincePressed));
+    }
+
+    float EvaluateCurve(float time)
+    {
+        return m_AnimationCurve.Evaluate(time);
     }
 }
